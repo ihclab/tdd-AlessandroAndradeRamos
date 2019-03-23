db490ff29630f889bef3b215106c572d2706bf63
AlessandroAndradeRamos <aandrade8@ucol.mx>
2019-03-22 19:56:24 -0600

    Se agregó el resto de casos de prueba incluidos en las diapositivas

--- Ordenamiento.pruebas/PruebasOrdenamiento.cs
+++ Ordenamiento.pruebas/PruebasOrdenamiento.cs
@@ -10,22 +10,34 @@ namespace Ordenamiento.pruebas
         private Ordenamiento.burbuja ordenar = new Ordenamiento.burbuja();
 
         [TestMethod]
-        public void TestInvertido(){
-            int[] entrada = new int[]{5, 4, 3, 2, 1};
-            ordenar.Ordenar(entrada);
-            CollectionAssert.AreEqual(salidaEsperada1, entrada);
-        }
-        [TestMethod]
         public void TestOrdenado(){
             int[] entrada = new int[]{1, 2, 3, 4, 5};
             ordenar.Ordenar(entrada);
             CollectionAssert.AreEqual(salidaEsperada1, entrada);
         }
         [TestMethod]
-        public void TestRepetido(){
-            int[] entrada = new int[]{4, 1, 2, 3, 4};
+        public void TestDesordenado(){
+            int[] entrada = new int[]{1, 3, 2, 4, 5};
             ordenar.Ordenar(entrada);
-            CollectionAssert.AreEqual(salidaEsperada3, entrada);
+            CollectionAssert.AreEqual(salidaEsperada1, entrada);
+        }
+        [TestMethod]
+        public void TestUltimoPrincipio(){
+            int[] entrada = new int[]{5, 1, 2, 3, 4};
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada1, entrada);
+        }
+        [TestMethod]
+        public void TestPrimeroFinal(){
+            int[] entrada = new int[]{2, 3, 4, 5, 1};
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada1, entrada);
+        }
+        [TestMethod]
+        public void TestInvertido(){
+            int[] entrada = new int[]{5, 4, 3, 2, 1};
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada1, entrada);
         }
     }
 }
